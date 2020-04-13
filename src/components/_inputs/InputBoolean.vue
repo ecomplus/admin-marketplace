6ef2bb8ffc209d7a048a57132098eca061e791e2
Leonardo Matos <leomp120894@gmail.com>
2020-04-13 16:05:50 -0300

    fix(input-boolean): warning color to disable bool, set :defaultChecked

--- src/components/_inputs/InputBoolean.vue
+++ src/components/_inputs/InputBoolean.vue
@@ -1,6 +1,8 @@
 <template>
   <a-switch
     :name="name"
+    :class="localValue && name.indexOf('disable') > -1 ? 'bg-warning' : null"
+    :defaultChecked="Boolean(schema.default)"
     v-model="localValue"
   />
 </template>
