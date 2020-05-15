bb6d2f43d02988a3b29d06de9c89c1e8695118fe
Leonardo Matos <leomp120894@gmail.com>
2020-05-14 23:02:31 -0300

    fix(money-input-matcher): check exact field name or schema 'maximum'

--- src/lib/_custom-input-matchers/money.js
+++ src/lib/_custom-input-matchers/money.js
@@ -1,6 +1,21 @@
 import InputMoney from './../../components/_inputs/InputMoney.vue'
 
 export default (field, localSchema) => {
-  return (field.includes('amount') || field.includes('price')) &&
-    InputMoney
+  let isValidField
+  switch (field) {
+    case 'amount':
+    case 'price':
+    case 'total_price':
+    case 'cost':
+      isValidField = true
+      break
+    default:
+      if (
+        (field.includes('amount') || field.includes('price') || field.includes('cost')) &&
+        (localSchema.maximum === undefined || localSchema.maximum > 100)
+      ) {
+        isValidField = true
+      }
+  }
+  return isValidField && InputMoney
 }
