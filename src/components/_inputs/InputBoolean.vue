d40a1a71dd84d5e30ec661a6f9cb57ddedb511fd
Fabio <fabioguieiro@gmail.com>
2020-10-06 20:55:31 -0300

    refactor(ec-admin-settings-form): replacing antd with bootstrap 4 + improvements

--- src/components/_inputs/InputBoolean.vue
+++ src/components/_inputs/InputBoolean.vue
@@ -7,7 +7,7 @@
     class="custom-control-input" 
     :id="schema.title"
     :class="localValue && name.indexOf('disable') > -1 ? 'bg-warning' : null"
-    :value="Boolean(schema.default)">
+  >
   <label class="custom-control-label" :for="schema.title">{{schema.title}}</label>
 </div>
 
