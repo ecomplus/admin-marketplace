a4ad5bc594bce0cdbb501a5eb9808b4251626a06
Fabio <fabioguieiro@gmail.com>
2020-10-08 15:55:47 -0300

    fix(zip-codejs): create component for initial and final zip code

--- src/lib/_custom-input-matchers/zip-code.js
+++ src/lib/_custom-input-matchers/zip-code.js
@@ -1,5 +1,10 @@
 import InputZipCode from './../../components/_inputs/InputZipCode.vue'
 
 export default (field, localSchema) => {
-  return field.includes('zip') && InputZipCode
+  if (field.includes('zip')){
+    return InputZipCode
+  }
+  else if (field.includes('inicial') || field.includes('final')){
+    return InputZipCode
+  }
 }
