526dab84018ee49b133f07b35ee78d44811662b3
Thiago Abreu Rodrigues <thiago.ar17@gmail.com>
2022-01-11 22:51:05 -0300

    feat: 🎸 Add option to import spreadsheet on import-feed app
    a spreadsheet import option has been added in the following example
    
    BREAKING CHANGE: 🧨 you will need to reinstall the feed importer app
    
    ✅ Closes: #13

---
+++ src/views/apps/AppFeedImporter.vue
@@ -0,0 +1,49 @@
+<template>
+  <Application>
+    <template #settings-append>
+      <div id="collapse-settings" class="collapse show">
+        <div>
+          <form autocomplete="off">
+            <div role="tablist" class="card border mb-2">
+              <div id="collapse-0" class="card-body collapse show">
+                <div>
+                  <div>
+                    <div class="form-group">
+                      <h3>Importação de planílha (CSV/XLSX)</h3>
+                      <span
+                        >Importação baseada no seguinte
+                        <a
+                          href="https://firebasestorage.googleapis.com/v0/b/ecom-feed-importer.appspot.com/o/public%2Ftemplate.csv?alt=media&token=85526ea3-ce75-4724-8184-cbae766e78fa"
+                        >
+                          exemplo.
+                        </a>
+                      </span>
+                      <b-form-file
+                        v-model="file"
+                        :state="Boolean(file)"
+                        placeholder="Escolha o arquivo (csv/xlsx)..."
+                        drop-placeholder="Drop file here..."
+                      />
+                    </div>
+                    <button class="btn btn-primary" type="submit" @click="uploadFile()">
+                      <span class="d-inline-block" style="width: 1.15rem">
+                        <span>
+                          <i class="fa fa-floppy-o"></i>
+                        </span>
+                      </span>
+                      <slot name="submit-label">
+                        {{ i19save }}
+                      </slot>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </template>
+  </Application>
+</template>
+
+<script src="./js/AppFeedImporter.js"></script>
---
+++ src/views/apps/js/AppFeedImporter.js
@@ -0,0 +1,77 @@
+
+import { BFormFile } from 'bootstrap-vue'
+import Application from '../../Application.vue'
+import axios from 'axios'
+import { $ecomConfig, i18n } from '@ecomplus/utils'
+import ecomApps from '@ecomplus/apps-manager'
+
+import {
+  i19save
+} from '@ecomplus/i18n'
+
+const appClient = axios.create({
+  // baseURL: 'http://us-central1-ecom-feed-importer.cloudfunctions.net/app/ecom',
+  baseURL: 'http://localhost:5001/ecom-feed-importer/us-central1/app/feed-importer',
+  headers: {
+    'X-Store-Id': $ecomConfig.get('store_id')
+  }
+})
+
+export default {
+  name: 'AppFeedImporter',
+  data () {
+    return {
+      file: '',
+      applicationBody: {}
+    }
+  },
+
+  props: {
+    ecomApps: {
+      type: Object,
+      default: () => ecomApps
+    }
+  },
+
+  components: {
+    BFormFile,
+    Application
+  },
+
+  computed: {
+    i19save: () => i18n(i19save),
+  },
+  created () {
+    this.loadApplicationBody()
+  },
+
+  methods: {
+    async loadApplicationBody () {
+      const { ecomApps } = this
+      this.loading = true
+      let data = this.$route.params.objectId && await ecomApps.find(this.$route.params.objectId)
+      if (!data) {
+        const [app] = await ecomApps.list({
+          params: { app_id: this.$route.params.appId }
+        })
+        if (app && app._id) {
+          data = await ecomApps.find(app._id).data
+        }
+      }
+      if (data) {
+        Object.assign(this.applicationBody, data.data)
+      }
+    },
+
+    async uploadFile () {
+      const form = new FormData()
+      form.append('file', this.file)
+      const { hidden_data: hiddenData } = this.applicationBody
+      if (!hiddenData || hiddenData.__token) {
+        await this.loadApplicationBody()
+      }
+      console.log(hiddenData, this.applicationBody)
+      await appClient.post(`/upload?token=${hiddenData.__token}&store_id=${$ecomConfig.get('store_id')}`, form)
+    }
+  }
+}
